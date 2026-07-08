93e6f772feb3f02ad255e70b9b0968d7fb9a9503
jin <jinsu.kim1011@gmail.com>
2026-07-08 08:48:02 +0200

    feat(post-list): Create PostList component to display a list of posts

---
+++ src/components/post-list.tsx
@@ -0,0 +1,16 @@
+import { Post } from "@/types/post";
+import PostListItem from "@/components/post-list-item";
+
+type PostListProps = {
+  posts: Post[];
+};
+
+export default function PostList({ posts }: PostListProps) {
+  return (
+    <ul className="flex flex-col gap-0">
+      {posts.map((post) => (
+        <PostListItem post={post} key={post.id} />
+      ))}
+    </ul>
+  );
+}
