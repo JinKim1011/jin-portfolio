c4e2f5109f7fb4ebfb887ddb33ff0f1ece54cf86
jin <jinsu.kim1011@gmail.com>
2026-07-08 16:14:38 +0200

    fix(footer): add a unit in tailwind class name

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t-[0.5]">
+    <footer className="border-t-[0.5px]">
       <div className="container flex h-14 items-center text-sm text-muted-foreground">
         © {new Date().getFullYear()} Jin Kim
       </div>
