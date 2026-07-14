ee891b0f23671bc8c5b72abf317ddeed4dc96e7e
jin <jinsu.kim1011@gmail.com>
2026-07-14 08:03:31 +0200

    feat(typography): apply typography preset

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,10 @@
 export default async function Footer() {
   "use cache";
   return (
-    <footer className="border-t-[0.5px]">
-      <div className="container flex h-14 items-center text-sm text-muted-foreground">
-        © {new Date().getFullYear()} Jin Kim
-      </div>
+    <footer className="border-t-[0.5px] h-14 items-center flex">
+      <span className="text-caption">
+        © {new Date().getFullYear()} JINSU KIM
+      </span>
     </footer>
   );
 }
