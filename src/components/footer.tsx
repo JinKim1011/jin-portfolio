da04cd816fa231749fb1cab19707fc7c6eb7b04c
jin <jinsu.kim1011@gmail.com>
2026-07-15 09:32:48 +0200

    feat(footer): apply max width style classess to inner div

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,11 @@ export default async function Footer() {
   "use cache";
   return (
     <footer className="border-t-[0.5px] h-14 items-center flex">
-      <span className="text-caption">
-        © {new Date().getFullYear()} JINSU KIM
-      </span>
+      <div className="max-w-190 w-full px-5 mx-auto">
+        <span className="text-caption">
+          © {new Date().getFullYear()} JINSU KIM
+        </span>
+      </div>
     </footer>
   );
 }
