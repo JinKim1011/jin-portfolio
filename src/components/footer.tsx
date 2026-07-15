7e7eae2f8349fc333afb2e4a65005526f0e5a461
jin <jinsu.kim1011@gmail.com>
2026-07-15 14:23:14 +0200

    feat(footer): apply color token for border and update text color for muted effect

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
 export default async function Footer() {
   "use cache";
   return (
-    <footer className="border-t-[0.5px] h-14 items-center flex">
+    <footer className="border-t-[0.5px] border-stroke h-14 items-center flex">
       <div className="max-w-190 w-full px-5 mx-auto">
-        <span className="text-caption">
+        <span className="text-caption text-content-muted">
           © {new Date().getFullYear()} JINSU KIM
         </span>
       </div>
