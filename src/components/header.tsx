595ba9d4ae4391f98a0901331846cdae7102f97d
jin <jinsu.kim1011@gmail.com>
2026-07-15 09:32:54 +0200

    feat(header): apply max width style classess to inner div

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 border-b-[0.5px]">
-      <div className="container flex h-14 items-center justify-between">
+      <div className="flex h-14 items-center justify-between max-w-190 w-full px-5 mx-auto">
         <Link href="/" className="flex flex-col">
           <span className="text-label">JIN KIM</span>
           <span className="text-label">DESIGN ENGINEER</span>
