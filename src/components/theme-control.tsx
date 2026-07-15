b462bc763f3f1b519079f2125cfc3c4287558d39
jin <jinsu.kim1011@gmail.com>
2026-07-15 15:52:57 +0200

    feat(theme-control): add minimum ThemeControl component for theme switching functionality

---
+++ src/components/theme-control.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/util";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function ThemeContol() {
+  const { theme, setTheme } = useTheme();
+  const [mount, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mount) return null;
+
+  const options = ["system", "light", "dark"] as const;
+
+  return (
+    <div className="flex w-fit gap-1">
+      {options.map((option) => {
+        const isActive = theme === option;
+        return (
+          <button
+            key={option}
+            onClick={() => setTheme(option)}
+            className={cn(
+              "text-label-small",
+              isActive
+                ? "bg-surface-interactive-active text-content-interactive-active"
+                : "bg-surface-interactive text-content-interactive",
+            )}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
