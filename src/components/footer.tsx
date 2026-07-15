a77566f09fe56c3c9d8d09edb5e1b01e5bad265c
jin <jinsu.kim1011@gmail.com>
2026-07-15 15:53:02 +0200

    feat(footer): integrate ThemeControl component into footer for theme switching

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,14 @@
+import ThemeContol from "./theme-control";
+
 export default async function Footer() {
   "use cache";
   return (
-    <footer className="border-t-[0.5px] border-stroke h-14 items-center flex">
-      <div className="max-w-190 w-full px-5 mx-auto">
+    <footer className="border-stroke flex h-14 items-center border-t-[0.5px]">
+      <div className="mx-auto flex w-full max-w-190 justify-between px-5">
         <span className="text-caption text-content-muted">
           © {new Date().getFullYear()} JINSU KIM
         </span>
+        <ThemeContol />
       </div>
     </footer>
   );
