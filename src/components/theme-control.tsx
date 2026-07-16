537f4a79b19fee004a28bc5a8ecd90660ec02b5f
jin <jinsu.kim1011@gmail.com>
2026-07-16 09:51:43 +0200

    feat(theme-control): replace theme buttons with SegmentedControl component

--- src/components/theme-control.tsx
+++ src/components/theme-control.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { cn } from "@/lib/util";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import SegmentedControl from "./ui/segmented-control";
+import { LaptopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+
+type ThemeMode = "light" | "dark" | "system";
 
 export default function ThemeControl() {
   const { theme, setTheme } = useTheme();
@@ -12,29 +15,23 @@ export default function ThemeControl() {
 
   if (!mounted) return null;
 
-  const options = ["system", "light", "dark"] as const;
+  const options = [
+    { value: "system", iconName: LaptopIcon },
+    { value: "light", iconName: SunIcon },
+    { value: "dark", iconName: MoonIcon },
+  ];
+
+  const handleThemeChange = (selected: string) => {
+    if (selected === "light" || selected === "dark" || selected === "system") {
+      setTheme(selected as ThemeMode);
+    }
+  };
 
   return (
-    <div className="flex w-fit gap-1" role="radiogroup" aria-label="theme">
-      {options.map((option) => {
-        const isActive = theme === option;
-        return (
-          <button
-            key={option}
-            role="radio"
-            onClick={() => setTheme(option)}
-            aria-checked={isActive}
-            className={cn(
-              "text-label-small",
-              isActive
-                ? "bg-surface-interactive-active text-content-interactive-active"
-                : "bg-surface-interactive text-content-interactive",
-            )}
-          >
-            {option}
-          </button>
-        );
-      })}
-    </div>
+    <SegmentedControl
+      options={options}
+      onChange={handleThemeChange}
+      value={theme as ThemeMode}
+    />
   );
 }
