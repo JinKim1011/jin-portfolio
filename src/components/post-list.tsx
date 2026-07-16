19efd382d86ecc34843d440586b57afbb4b180ab
jin <jinsu.kim1011@gmail.com>
2026-07-16 16:23:58 +0200

    feat(post-list): pass view prop to PostListItem component

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { Post } from "@/types/post";
+import { Post, PostView } from "@/types/post";
 import PostListItem from "@/components/post-list-item";
 
 type PostListProps = {
   posts: Post[];
+  view: PostView;
 };
 
-export default function PostList({ posts }: PostListProps) {
+export default function PostList({ posts, view }: PostListProps) {
   return (
     <ul className="flex flex-col gap-0 pb-10">
       {posts.map((post) => (
-        <PostListItem post={post} key={post.id} />
+        <PostListItem post={post} key={post.id} view={view} />
       ))}
     </ul>
   );
