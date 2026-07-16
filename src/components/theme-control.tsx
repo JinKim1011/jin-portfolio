5df2124b710e96ee8e4adeaafd546c2c7930b2de
jin <jinsu.kim1011@gmail.com>
2026-07-16 17:14:51 +0200

    fix(theme-control): use default value for theme when undefined

--- src/components/theme-control.tsx
+++ src/components/theme-control.tsx
@@ -31,7 +31,7 @@ export default function ThemeControl() {
     <SegmentedControl
       options={options}
       onChange={handleThemeChange}
-      value={theme as ThemeMode}
+      value={theme ?? "system"}
       ariaLabel="theme-control"
     />
   );
