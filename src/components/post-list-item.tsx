09d9a244d65ae436c0500f6e7ed12c0bac3d1b0e
JinKim1011 <jinsu.kim1011@gmail.com>
2026-07-28 19:35:56 +0200

    fix(post-list-item): update base hover, active, and press-effect to be canceled when pointer over tag-wrapper

--- src/components/post-list-item.tsx
+++ src/components/post-list-item.tsx
@@ -44,7 +44,7 @@ function getPostLink(post: Post) {
 }
 
 const ItemBase =
-  "group relative isolate flex bg-surface-interactive hover:bg-surface-interactive-hover active:bg-surface-interactive-active press-effect";
+  "group relative isolate flex bg-surface-interactive hover:bg-surface-interactive-hover active:bg-surface-interactive-active press-effect has-[.tag-wrapper:hover]:!bg-surface-interactive has-[.tag-wrapper:active]:!bg-surface-interactive has-[.tag-wrapper:active]:!transform-none";
 
 const listItemBase =
   "items-center justify-between px-2.5 -mx-2.5 w-[calc(100%+1.25rem)]";
