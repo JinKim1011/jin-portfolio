d27ae8d23b53b894a54e23fbdc5459b4de94a7fb
JinKim1011 <jinsu.kim1011@gmail.com>
2026-07-29 09:12:15 +0200

    feat(config): add image remote patterns for Notion hosted file URL domains

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "secure.notion-static.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.notion.so",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
