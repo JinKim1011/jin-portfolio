b5161c74a7defd1b6ed9909f718206c6411e9071
jin <jinsu.kim1011@gmail.com>
2026-08-17 23:19:53 +0200

    feat(PostBlocks): add interfaces for StaticBlock, CodeBlock, and ImageBlock

--- src/types/post.ts
+++ src/types/post.ts
@@ -23,3 +23,31 @@ export interface PostDetail extends Post {
 }
 
 export type PostView = "list" | "card";
+
+export interface StaticBlock {
+  id: string;
+  type:
+    | "paragraph"
+    | "heading_1"
+    | "heading_2"
+    | "heading_3"
+    | "bulleted_list_item"
+    | "numbered_list_item"
+    | "quote";
+  html: string;
+}
+
+export interface CodeBlock {
+  id: string;
+  type: "code";
+  highlightedHtml: string;
+  raw: string;
+}
+
+export interface ImageBlock {
+  id: string;
+  type: "image";
+  src: string;
+  alt: string;
+  caption: string;
+}
