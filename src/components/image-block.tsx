bf361e08f1d042c90ebcd351108a5c29372659c3
jin <jinsu.kim1011@gmail.com>
2026-08-18 12:34:52 +0200

    refactor(ImageBlock): restructure imageStyle for improved loading state handling

--- src/components/image-block.tsx
+++ src/components/image-block.tsx
@@ -1,10 +1,30 @@
+"use client";
+
 import Image from "next/image";
+import { ImageIcon } from "./icons";
+import { useState } from "react";
+import { cva } from "class-variance-authority";
 
 type ImageBlockProps = {
   src: string;
   alt: string;
   caption: string;
 };
+
+const imageStyle = cva(
+  "border-stroke mb-3 h-auto w-full shrink-0 border-[0.5px] object-cover",
+  {
+    variants: {
+      loading: {
+        true: "bg-surface-muted",
+        false: "bg-none",
+      },
+    },
+    defaultVariants: {
+      loading: true,
+    },
+  },
+);
 
 export function ImageBlock({ src, alt, caption }: ImageBlockProps) {
   const [failed, setFailed] = useState(false);
