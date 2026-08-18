b39a91631158f369d73b63eed07f32e7523941f8
jin <jinsu.kim1011@gmail.com>
2026-08-18 11:39:18 +0200

    refactor(CodeBlock): add code block owned style

--- src/components/code-block.tsx
+++ src/components/code-block.tsx
@@ -7,7 +7,7 @@ type CodeBlockProps = {
 
 export function CodeBlock({ html, raw }: CodeBlockProps) {
   return (
-    <div className="notion-code">
+    <div className="notion-code bg-surface-muted border-stroke relative mb-3 border-[0.5px]">
       <div dangerouslySetInnerHTML={{ __html: html }} />
       <div className="absolute top-3 right-3 z-10 inline-block">
         <CopyButton raw={raw} />
