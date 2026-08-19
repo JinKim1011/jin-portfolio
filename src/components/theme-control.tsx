a08d5a2532812b84b426f4d36f1922a86181ad9a
jin <jinsu.kim1011@gmail.com>
2026-08-20 00:39:08 +0200

    feat(ThemeControl): set default theme value to 'dark' for improved user experience

--- src/components/theme-control.tsx
+++ src/components/theme-control.tsx
@@ -31,7 +31,7 @@ export default function ThemeControl() {
     <SegmentedControl
       options={options}
       onChange={handleThemeChange}
-      value={theme ?? "system"}
+      value={theme ?? "dark"}
       ariaLabel="theme-control"
     />
   );
