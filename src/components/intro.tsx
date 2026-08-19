3e94f4358a74a446f3ffcd28a04042631d02f1d9
jin <jinsu.kim1011@gmail.com>
2026-08-19 15:24:43 +0200

    fix(Intro): adjust padding in the Intro component

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -6,7 +6,7 @@ import { MailIcon } from "./icons";
 
 export default function Intro() {
   return (
-    <div className="text-content-muted text-caption flex flex-col gap-3 pt-10 pb-8">
+    <div className="text-content-muted text-caption flex flex-col gap-3 pt-10 pb-6">
       <p>
         <span>I'M </span>
         <SocialLink
