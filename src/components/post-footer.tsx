af035372542e7bf1135c1a7777a17e9ca6b745e5
jin <jinsu.kim1011@gmail.com>
2026-08-24 23:41:42 +0200

    feat(PostFooter): add PostFooter component for displaying related posts

---
+++ src/components/post-footer.tsx
@@ -0,0 +1,25 @@
+import { Post } from "@/types/post";
+import RevealEffect from "./reveal-effect";
+import { ShareButton } from "./share-button";
+import PostList from "./post-list";
+
+type PostFooterProps = {
+  relatedPosts: Post[];
+};
+
+const footerWrapper = "flex flex-col gap-14";
+const relatedPost = "border-stroke flex w-full flex-col border-t-[0.5px]";
+const relatedPostTitle =
+  "text-label text-content-default w-fit pt-5 pb-2.5 select-none";
+
+export default function PostFooter({ relatedPosts }: PostFooterProps) {
+  return (
+    <RevealEffect className={footerWrapper} delay={0.3}>
+      <ShareButton />
+      <div className={relatedPost}>
+        <h2 className={relatedPostTitle}>RELATED POSTS</h2>
+        <PostList posts={relatedPosts} view="list" />
+      </div>
+    </RevealEffect>
+  );
+}
