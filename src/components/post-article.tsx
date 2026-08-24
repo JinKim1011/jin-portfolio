0025d6eb28ea55ab6abbb49b0822b7d9c880225f
jin <jinsu.kim1011@gmail.com>
2026-08-24 23:56:01 +0200

    feat(PostArticle): create PostArticle component for rendering post details and related posts

---
+++ src/components/post-article.tsx
@@ -0,0 +1,24 @@
+import { Post, PostDetail } from "@/types/post";
+import PostHeader from "./post-header";
+import PostCover from "./post-cover";
+import PostBody from "./post-body";
+import PostFooter from "./post-footer";
+
+type PostArticleProps = {
+  relatedPosts: Post[];
+  post: PostDetail;
+};
+
+export default function PostArticle({ post, relatedPosts }: PostArticleProps) {
+  return (
+    <article className="flex flex-col gap-14 pt-16 pb-14 font-sans">
+      <PostHeader post={post} />
+
+      <PostCover post={post} />
+
+      <PostBody post={post} />
+
+      <PostFooter relatedPosts={relatedPosts} />
+    </article>
+  );
+}
