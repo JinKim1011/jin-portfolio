dcad95c84d22a1bd66e840d3e0b4b0a4f8cc85e5
jin <jinsu.kim1011@gmail.com>
2026-09-08 20:09:43 +0900

    feat(callout): update CalloutBlock styles and change label from INFO to NOTE

--- src/components/callout-block.tsx
+++ src/components/callout-block.tsx
@@ -6,10 +6,10 @@ type CallouyBlockProps = {
 
 export default function CalloutBlock({ html }: CallouyBlockProps) {
   return (
-    <div className="border-stroke flex flex-col gap-2 border-t-[0.5] border-b-[0.5px] py-8">
-      <div className="flex items-center gap-2">
+    <div className="border-stroke flex flex-col gap-3 border-t-[0.5] border-b-[0.5px] py-8">
+      <div className="flex items-center gap-1.5">
         <TipIcon aria-hidden className="text-content-feedback-warning size-4" />
-        <span className="text-caption text-content-feedback-warning">INFO</span>
+        <span className="text-caption text-content-feedback-warning">NOTE</span>
       </div>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </div>
