adbc92f9c97b72090793a6cf8d9e40669b25080d
jin <jinsu.kim1011@gmail.com>
2026-09-08 20:02:24 +0900

    feat(post): include CalloutBlock in PostBlock type

--- src/types/post.ts
+++ src/types/post.ts
@@ -12,7 +12,7 @@ export interface Post {
   externalUrl: string | null;
 }
 
-export type PostBlock = StaticBlock | CodeBlock | ImageBlock;
+export type PostBlock = StaticBlock | CodeBlock | ImageBlock | CalloutBlock;
 
 export interface PostDetail extends Post {
   blocks: PostBlock[];
