85b22e7286f6e948f2c4b0385b96e76bbb1aae04
jin <jinsu.kim1011@gmail.com>
2026-09-14 12:48:19 +0200

    refactor(segmented-control): update import path for IconComponent

--- src/components/ui/segmented-control.tsx
+++ src/components/ui/segmented-control.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { IconComponent } from "../icons/icon";
+import type { IconComponent } from "@/components/icons/icon";
 import { cn } from "@/lib/utils/cn";
 
 type SegmentOption = {
