780c26d1b58f8b9aac035a6ac7b54426035f83a7
jin <jinsu.kim1011@gmail.com>
2026-09-14 11:50:03 +0200

    feat(video): wrap iframe in figure element and add figcaption elements for showing caption

--- src/components/video-block.tsx
+++ src/components/video-block.tsx
@@ -5,13 +5,18 @@ type VideoBlockProps = {
 
 export function VideoBlock({ src, caption }: VideoBlockProps) {
   return (
-    <iframe
-      src={src}
-      title={caption || "Embedded video"}
-      className="mb-5 aspect-video w-full"
-      loading="lazy"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
+    <figure className="mb-5">
+      <iframe
+        src={src}
+        title={caption || "Embedded video"}
+        className="aspect-video w-full"
+        loading="lazy"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+      {caption ? (
+        <figcaption className="text-caption mt-1">{caption}</figcaption>
+      ) : null}
+    </figure>
   );
 }
